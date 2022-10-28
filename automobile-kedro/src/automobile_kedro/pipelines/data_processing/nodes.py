60478d0d09a94fd91b894021365e9d5b345b029f
Renato Melo <rdemeloneto2@gmail.com>
2022-10-28 17:39:46 -0300

    Included scaling node

--- automobile-kedro/src/automobile_kedro/pipelines/data_processing/nodes.py
+++ automobile-kedro/src/automobile_kedro/pipelines/data_processing/nodes.py
@@ -5,6 +5,7 @@ generated using Kedro 0.18.3
 import pandas as pd
 from typing import Dict
 from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import MinMaxScaler
 
 def preprocess_encoding(df: pd.DataFrame):
     "This function converts categorical features into numeric features"
@@ -16,4 +17,12 @@ def preprocess_encoding(df: pd.DataFrame):
              'horsepower', 'peak-rpm', 'price'     
            ] 
     df[features]= ordinal_encoder.fit_transform(df[features])
-    return df, ordinal_encoder
+    return df
+
+def preprocess_scaling(df: pd.DataFrame):
+    "This function scales all numerical values in a range between 0-1"
+    scaler = MinMaxScaler()
+    df_NParray = scaler.fit_transform(df)
+    df_scaled = pd.DataFrame(df_NParray, columns=df.columns)
+    return df_scaled       
+  
--- automobile-kedro/src/automobile_kedro/pipelines/data_processing/pipeline.py
+++ automobile-kedro/src/automobile_kedro/pipelines/data_processing/pipeline.py
@@ -4,6 +4,7 @@ generated using Kedro 0.18.3
 """
 
 from automobile_kedro.pipelines.data_processing.nodes import preprocess_encoding
+from automobile_kedro.pipelines.data_processing.nodes import preprocess_scaling
 from kedro.pipeline import Pipeline, node, pipeline
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -13,5 +14,11 @@ def create_pipeline(**kwargs) -> Pipeline:
             inputs = 'automobile',
             outputs = 'encoded_automobile',
             name = 'enconding_automobile_node',
+        ),
+        node(
+            func= preprocess_scaling,
+            inputs = 'encoded_automobile',
+            outputs = 'scaled_automobile',
+            name = 'scaling_automobile_node',
         ),
     ])
