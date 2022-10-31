dfffb5b340af4b91ade63bc08b97ed47e037b368
Renato Melo <rdemeloneto2@gmail.com>
2022-10-31 17:21:18 -0300

    Data science pipeline added

--- automobile-kedro/src/automobile_kedro/pipeline_registry.py
+++ automobile-kedro/src/automobile_kedro/pipeline_registry.py
@@ -4,6 +4,7 @@ from typing import Dict
 from kedro.pipeline import Pipeline
 
 from automobile_kedro.pipelines import data_processing as dpp
+from automobile_kedro.pipelines import data_science as dsc
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -13,8 +14,12 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
     pre_processing_pipeline = dpp.create_pipeline()
+    data_science_pipeline = dsc.create_pipeline()
+
 
     return {
         "dpp":pre_processing_pipeline,
-        "__default__": pre_processing_pipeline 
+        "dsc":data_science_pipeline,
+        "__default__": pre_processing_pipeline
+                       + data_science_pipeline 
     }
